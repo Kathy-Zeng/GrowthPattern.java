3d8265343f1ca03a79c9e1a623d21a6aedb59496
Kathy-Zeng <70964239+Kathy-Zeng@users.noreply.github.com>
2021-01-13 21:38:35 -0800

    Update GrowthPattern.java

--- GrowthPattern.java
+++ GrowthPattern.java
@@ -1,9 +1,7 @@
 // Kathy Zeng
 // 1/12/21
 // GrowthPattern.java
-// Description: 
-
-import java.util.Scanner;
+// Description: The program calculates a plant's rate.
 
 public class GrowthPattern
 {
@@ -20,47 +18,60 @@ public class GrowthPattern
 		gp.growIt();
 	}
 	
-	//
+	// In growIt method, prints a welcome message and call other methods. 
+	// decideSymbols and printSymbols method call in a for loop and printRate
+	// calls outside of the loop. 
 	public void growIt()
 	{
-		decideSymbols('?');
-		printSymbols(1);
+		System.out.println("\n\n\n");
+		System.out.print("Welcome to GrowthPattern.java.");
+		char[] sign = decideSymbols();
+		printSymbols(sign);
 		printRate();
 	}
 
-	//
-	public void decideSymbols(char sign)
+	// In decideSymbols method, '-' is use for when height < 0/result in negative change.
+	// 'o' is use for when rate is not changing. '+' is use when rate result in positive 
+	// change. 
+	public char[] decideSymbols()
 	{
-		if(height[] < 0)
-			sign = '-';
-		else if(height[] == 1)
-			sign = 'o';
-		else
-			sign = '+';
-	}
-	
-	//
-	public void printSymbols(int time)
-	{
-		System.out.println("\n\n\n");
-		for(int i = 0; i < height[]; i++)
+		char[] sign = new char[height.length];
+		sign[0] = 'o';
+		for(int c = 1; c < height.length; c++)
 		{
-			System.out.printf("t%5d%s", time);
-	    }
+			if(height[c] < height[c - 1])
+				sign[c] = '-';
+			else if(height[c] == height[c - 1])
+				sign[c] = 'o';
+			else
+				sign[c] = '+';
+		}
+		return sign; 
 	}
 	
-	//
+	// Pass in parameters for time and signs. Nested loop is use for printing
+	// time vertically and height horizontally.
+	public void printSymbols(char[] sign)
+	{
+		for(int j = 0; j < height.length; j++)
+		{
+			System.out.printf("%nt%-5d", j);
+			int symbolNum = height[j] + 5;
+	    for(int k = 0; k < symbolNum; k++)
+		{
+			System.out.printf("%c", sign[j]);
+	    }
+	  }
+    }
+	// Prints a result for rate.
 	public void printRate()
 	{
-		int rate = 0;
-		int cHeight = 0;
-		Scanner s = new Scanner(System.in);
-		System.out.print("Welcome to GrowthPatterm.java. Please enter a height: ");
-		height = s.nextInt();
-		System.out.println();
-		System.out.printf("This is the growth rate " + rate + "mm/unit time.");
+		double rate = 0.0;
+		double cHeight = 0;
 		cHeight = height[height.length - 1] - height[0];
 		rate = cHeight/height.length - 1;
+		System.out.println("\n");
+		System.out.printf("This is the growth rate is %.2f mm per unit time.", rate);
 		System.out.println("\n\n\n");
 	}
 }
